54984822d692ffe5f8d464282a92204c82fbf0e9
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-02-21 11:16:07 +0800

    Add files via upload

---
+++ q2_calc_cylinder_volume (1).py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#finding area
+radius = float(input("radius"))
+from math import pi
+area = radius * radius * pi
+
+#finding volume
+length = float(input("length"))
+volume = area * length
+print(volume)
+
+
+# In[ ]:
+
+
+
+
