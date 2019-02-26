1b779c27374834a8c1a7e2277423154651c5d2ba
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-02-26 10:45:37 +0800

    Add files via upload

---
+++ q01_check_even.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+n = int(input("enter number: "))
+
+if n % 2 == 0:
+    print(str(n) + " is even")
+else:
+    print(str(n) + " is odd")
+
+
+# In[ ]:
+
+
+
+
