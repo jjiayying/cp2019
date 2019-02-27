ce210c389d3632513477f09187a551fe87a0356f
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-02-27 09:24:01 +0800

    Add files via upload

---
+++ Practical 2/q06_kilograms_to_pounds(not done).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[36]:
+
+
+kilogram = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in kilogram:
+    print(number)
+    
+pound = [i * 2.2 for i in kilogram]
+print(pound)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
