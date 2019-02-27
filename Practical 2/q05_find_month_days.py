06d67094d77083f1527ace22518b26cd780986e0
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-02-27 09:01:17 +0800

    Add files via upload

---
+++ Practical 2/q05_find_month_days.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+from calendar import monthrange
+
+year = int(input("year please: "))
+month = int(input("month please: "))
+
+monthrange(int(year), int(month))
+
+
+# In[ ]:
+
+
+
+
