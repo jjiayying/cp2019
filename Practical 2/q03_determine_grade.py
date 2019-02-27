fb173c44e2dec3ddd13556d64fdf7f6296da35ca
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-02-27 08:41:14 +0800

    Add files via upload

---
+++ Practical 2/q03_determine_grade.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+Grade = int(input("Your Score Pleasee: "))
+
+if 70 <= Grade <= 100 :
+    print("A")
+elif 60 <= Grade <=69 :
+    print("B")
+elif 55 <= Grade <= 59 :
+    print("C")
+elif 50 <= Grade <= 54:
+    print("D")
+elif 45 <= Grade <= 49:
+    print("E")
+elif 35 <= Grade <= 44:
+    print("S")
+else:
+    print("U")
+
+
+# In[ ]:
+
+
+
+
