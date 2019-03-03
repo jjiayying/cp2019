bfebd1d72d03cb2eaff8224759f159cf2c173041
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-03-03 22:29:48 +0800

    Add files via upload

---
+++ Practical 2/_q12_find_factors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def print_factors(x):
+
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = int(input("number: "))
+
+print_factors(num)
+
+
+# In[ ]:
+
+
+
+
