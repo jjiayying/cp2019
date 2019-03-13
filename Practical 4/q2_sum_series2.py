30a333396505b2e3da7f8b01ae9d721f1112851c
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-03-13 08:56:29 +0800

    Add files via upload

---
+++ Practical 4/q2_sum_series2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def sum_series2(i):
+    sum_series = 0
+    for i in range(1,4):
+        sum_series += (i)/(2*i+1)
+        print(sum_series)
+        
+sum_series2(1)   
+
+
+# In[5]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
