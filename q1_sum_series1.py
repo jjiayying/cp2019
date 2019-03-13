17dab081d715a8fa7f77992ba276a61f8275123d
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-03-13 08:49:43 +0800

    Add files via upload

---
+++ q1_sum_series1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+def sum_series1(i):
+    sum_series = 1
+    for i in range(1,4):
+        sum_series += (1)/(i+1)
+        print(sum_series)
+        
+sum_series1(1)        
+
+
+# In[ ]:
+
+
+
+
