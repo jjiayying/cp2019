3bd5ba82f510c9e9c53aa10f7e11e7cf191853e4
jjiayying <47766337+jjiayying@users.noreply.github.com>
2019-03-19 21:48:30 +0800

    Add files via upload

---
+++ Practical 4/q7_find_largest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+alist = [5, 1, 8, 7, 10]
+def find_largest(alist):
+    alist.sort()
+    return alist[-1]
+
+find_largest(alist)
+
+
+# In[ ]:
+
+
+
+
